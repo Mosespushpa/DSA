76d277b1342990a3762fcd9470eefa48bbf09223
Moses <mosespushpa21082002@gmail.com>
2026-01-29 22:08:30 +0530

    Day(29-01-2026) Selection

--- Sorting/Selection_Sort.java
+++ Sorting/Selection_Sort.java
@@ -4,19 +4,31 @@ class Selection_Sort{
     public static void main(String ar[]){
         int arr[] = {3,4,5,7,6,3,2};
         int n = arr.length;
-        //Bubble Sort
+        //Selection Sort - 29-01-2026
         for(int i=0;i<n;i++){
-            int minId = i;
+            int id = i;
             for(int j=i+1;j<n;j++){
-                if(arr[j] < arr[minId]){
-                    minId = j;
-                  }
+                if(arr[j] < arr[id]){
+                    id = j;
+                }
             }
-            int temp = arr[minId];
-                    arr[minId] = arr[i];
-                    arr[i] = temp;  
-
+            int temp = arr[id];
+            arr[id] = arr[i];
+            arr[i] = temp;
         }
+
+        // for(int i=0;i<n;i++){
+        //     int minId = i;
+        //     for(int j=i+1;j<n;j++){
+        //         if(arr[j] < arr[minId]){
+        //             minId = j;
+        //           }
+        //     }
+        //     int temp = arr[minId];
+        //             arr[minId] = arr[i];
+        //             arr[i] = temp;  
+
+        // }
         for(int i=0;i<n;i++){
 
             System.out.print(arr[i]+" ");
