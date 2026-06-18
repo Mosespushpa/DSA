48e801633a26bde3fc2544900abab3b74c97cca4
Moses <mosespushpa21082002@gmail.com>
2026-06-18 12:47:17 +0530

    (18-06-2026) RESTART again from Sorting

--- Sorting/Bubble_Sort.java
+++ Sorting/Bubble_Sort.java
@@ -1,23 +1,40 @@
-import java.util.*;
-
 class Bubble_Sort{
-    public static void main(String ar[]){
-        int arr[] = {2,3,4,5,7,6,54,3,4,6,7};
-        int n = arr.length;
-        //Bubble Sort
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n-1;j++){
-                if(arr[j] > arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;                }
-            }
 
-        }
+    public void print(int n, int[] arr){
         for(int i=0;i<n;i++){
-
             System.out.print(arr[i]+" ");
         }
+        System.out.println();
+    }
+
+    public static void main(String mos[]){
+        Bubble1 b1 = new Bubble1();
+
+        int arr[] = {7,12,9,11,3};
+        int n = arr.length;
+        System.out.println("Before Sorting:");
+        //printing of the array
+        b1.print(n,arr);
+        //Bubble sort
+        for(int i=0;i<n;i++){
+            boolean isswap = false;
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j+1] < arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    isswap = true;
+                }
+            }
+            System.out.print("Pass " + (i+1) + ": ");
+            b1.print(n, arr);
+
+            if(!isswap) break;
+        }
         
+        System.out.println("After Sorting");
+        //printing of the array
+        b1.print(n,arr);
+
     }
 }
