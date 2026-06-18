531c1f31d5082fa6b34095844b6f15e08d457556
Moses <mosespushpa21082002@gmail.com>
2026-06-18 21:08:25 +0530

    Selection and Insertion Sorting Algorithm completed

--- Sorting/Insertion_Sort.java
+++ Sorting/Insertion_Sort.java
@@ -1,24 +1,33 @@
 import java.util.*;
-
 class Insertion_Sort{
-    public static void main(String ar[]){
-        int arr[] = {3,4,5,7,6,3,2};
-        int n = arr.length;
-        //Bubble Sort
-        for(int i=1;i<n;i++){
-            int j=i-1;
-            int temp = arr[i];
-            while(j>=0 && arr[j]>temp){
-                arr[j+1] = arr[j];
-                j = j-1;
-            }
-            arr[j+1] = temp;
 
-        }
+    public void print(int n, int arr[]){
         for(int i=0;i<n;i++){
-
             System.out.print(arr[i]+" ");
         }
-        
+        System.out.println();
+    }
+
+    public static void main(String mos[]){
+
+        int arr[] = {7,12,9,11,3};
+        int n = arr.length;
+
+        Insertion_Sort s1 = new Insertion_Sort();
+
+        System.out.println("Before Sorting");
+        s1.print(n,arr);
+
+        for(int i=1;i<n;i++){
+           int j = i-1;
+           int current_elem = arr[i];
+           while(j>=0 && arr[j] > current_elem){
+                arr[j+1] = arr[j];
+                j = j-1;
+           }
+           arr[j+1] = current_elem;
+        }
+        System.out.println("After Sorting");
+        s1.print(n,arr);
     }
 }
--- Sorting/Selection_Sort.java
+++ Sorting/Selection_Sort.java
@@ -1,38 +1,35 @@
 import java.util.*;
-
 class Selection_Sort{
-    public static void main(String ar[]){
-        int arr[] = {3,4,5,7,6,3,2};
-        int n = arr.length;
-        //Selection Sort - 29-01-2026
+
+    public void print(int n, int arr[]){
         for(int i=0;i<n;i++){
-            int id = i;
-            for(int j=i+1;j<n;j++){
-                if(arr[j] < arr[id]){
-                    id = j;
-                }
-            }
-            int temp = arr[id];
-            arr[id] = arr[i];
-            arr[i] = temp;
-        }
-
-        // for(int i=0;i<n;i++){
-        //     int minId = i;
-        //     for(int j=i+1;j<n;j++){
-        //         if(arr[j] < arr[minId]){
-        //             minId = j;
-        //           }
-        //     }
-        //     int temp = arr[minId];
-        //             arr[minId] = arr[i];
-        //             arr[i] = temp;  
-
-        // }
-        for(int i=0;i<n;i++){
-
             System.out.print(arr[i]+" ");
         }
-        
+        System.out.println();
+    }
+
+    public static void main(String mos[]){
+
+        int arr[] = {7,12,9,11,3};
+        int n = arr.length;
+
+        Selection_Sort s1 = new Selection_Sort();
+
+        System.out.println("Before Sorting");
+        s1.print(n,arr);
+
+        for(int i=0;i<n-1;i++){
+            int minIndex = i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        System.out.println("After Sorting");
+        s1.print(n,arr);
     }
 }
